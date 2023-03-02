661fd85d240e79abe104da56e850e24b233592ae
newbandname <techxtech@protonmail.com>
2023-03-02 17:31:42 -0600

    v3

--- powershell/server_discovery.ps1
+++ powershell/server_discovery.ps1
@@ -28,7 +28,7 @@ function Get-SystemInfo {
     $netInfo = Get-NetAdapter | Select-Object Name, InterfaceDescription, Status
     
     # Get IP configuration
-    $ipConfig = Get-NetIPAddress
+    $ipConfig = Get-NetIPAddress -AddressFamily IPv4 | Select-Object InterfaceAlias, IPAddress, PrefixLength, AddressState
     
     # Get installed software
     $software = Get-WmiObject -Class Win32_Product | Select-Object Name, Version
@@ -65,10 +65,43 @@ function Get-SystemInfo {
 # Call the Get-SystemInfo function
 $systemInfo = Get-SystemInfo
 
-# Convert system information to HTML table
-$table = $systemInfo | ConvertTo-Html -Fragment
+# Create HTML table for OS information
+$osTable = $systemInfo.OSInfo | ConvertTo-Html -Fragment -As Table -Property Caption, Version, BuildNumber, OSArchitecture
 
-# Create HTML template
+# Create HTML table for RAM information
+$ramTable = $systemInfo.RAMInfo | ConvertTo-Html -Fragment -As Table -Property RAM, Count
+
+# Create HTML table for CPU information
+$cpuTable = $systemInfo.CPUInfo | ConvertTo-Html -Fragment -As Table -Property Name, MaxClockSpeed, Caption, NumberOfCores, NumberOfLogicalProcessors
+
+# Create HTML table for logical disk information
+$diskTable = $systemInfo.DiskInfo | ConvertTo-Html -Fragment -As Table -Property DeviceID, MediaType, Size, FreeSpace
+
+# Create HTML table for file share information
+$shareTable = $systemInfo.ShareInfo | ConvertTo-Html -Fragment -As Table -Property Name, Path, Description, CurrentUsers, ShareState
+
+# Create HTML table for page file information
+$pageFileTable = $systemInfo.PageFileInfo | ConvertTo-Html -Fragment -As Table -Property Name, CurrentUsage
+
+# Create HTML table for network interface information
+$netTable = $systemInfo.NetInfo | ConvertTo-Html -Fragment -As Table -Property Name, InterfaceDescription, Status
+
+# Create HTML table for IP configuration information
+$ipTable = $systemInfo.IPConfig | ConvertTo-Html -Fragment -As Table -Property InterfaceAlias, IPAddress, PrefixLength, AddressState
+
+# Create HTML table for installed software
+$softwareTable = $systemInfo.InstalledSoftware | ConvertTo-Html -Fragment -As Table -Property Name, Version
+
+# Create HTML table for installed services
+$servicesTable = $systemInfo.InstalledServices | ConvertTo-Html -Fragment -As Table -Property Name, DisplayName, Status, StartType, Description
+
+# Create HTML table for installed roles
+$rolesTable = $systemInfo.InstalledRoles | ConvertTo-Html -Fragment -As Table -Property Name, InstallState
+
+# Create HTML table for IIS site configurations
+$iisTable = $systemInfo.IISConfig | ConvertTo-Html -Fragment -As Table -Property Name, PhysicalPath, Bindings
+
+# Combine HTML tables into final report
 $html = @"
 <html>
 <head>
@@ -93,7 +126,34 @@ $html = @"
 </head>
 <body>
     <h1>System Information Report</h1>
-    $table
+    <h2>Computer Information</h2>
+    <table>
+        <tr><th>Computer Name</th><td>$($systemInfo.ComputerName)</td></tr>
+    </table>
+    <h2>Operating System Information</h2>
+    $osTable
+    <h2>RAM Information</h2>
+    $ramTable
+    <h2>CPU Information</h2>
+    $cpuTable
+    <h2>Logical Disk Information</h2>
+    $diskTable
+    <h2>File Share Information</h2>
+    $shareTable
+    <h2>Page File Information</h2>
+    $pageFileTable
+    <h2>Network Interface Information</h2>
+    $netTable
+    <h2>IP Configuration Information</h2>
+    $ipTable
+    <h2>Installed Software</h2>
+    $softwareTable
+    <h2>Installed Services</h2>
+    $servicesTable
+    <h2>Installed Roles</h2>
+    $rolesTable
+    <h2>IIS Site Configurations</h2>
+    $iisTable
 </body>
 </html>
 "@
